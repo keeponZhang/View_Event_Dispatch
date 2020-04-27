2c08178f322246eba7343f0f7caf67530957e07c
zhangwengao <zhangwengao@yy.com>
2020-04-27 18:09:51 +0800

    no message

--- app/src/main/java/com/keepon/eventdispatch/ListenerActivity.java
+++ app/src/main/java/com/keepon/eventdispatch/ListenerActivity.java
@@ -2,9 +2,17 @@ package com.keepon.eventdispatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.ActionMode;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.accessibility.AccessibilityEvent;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -49,10 +57,123 @@ public class ListenerActivity extends Activity implements View.OnTouchListener, 
 
 //        mButton.getParent().requestDisallowInterceptTouchEvent(false);
         getWindow().getDecorView().setOnTouchListener(this);
+        // getWindow().setCallback(getCallback());
 //        mButton.setOnTouchListener(this);
 
 //        mLayout.setOnClickListener(this);
        mButton.setOnClickListener(this);
+    }
+
+    private Window.Callback getCallback() {
+        return new Window.Callback() {
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent event) {
+                return false;
+            }
+
+            @Override
+            public boolean dispatchKeyShortcutEvent(KeyEvent event) {
+                return false;
+            }
+
+            @Override
+            public boolean dispatchTouchEvent(MotionEvent event) {
+                Log.e(TAG, "getWindow().setCallback dispatchTouchEvent:");
+                return false;
+            }
+
+            @Override
+            public boolean dispatchTrackballEvent(MotionEvent event) {
+                return false;
+            }
+
+            @Override
+            public boolean dispatchGenericMotionEvent(MotionEvent event) {
+                return false;
+            }
+
+            @Override
+            public boolean dispatchPopulateAccessibilityEvent(AccessibilityEvent event) {
+                return false;
+            }
+
+            @Nullable
+            @Override
+            public View onCreatePanelView(int featureId) {
+                return null;
+            }
+
+            @Override
+            public boolean onCreatePanelMenu(int featureId, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onPreparePanel(int featureId, View view, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onMenuOpened(int featureId, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onMenuItemSelected(int featureId, MenuItem item) {
+                return false;
+            }
+
+            @Override
+            public void onWindowAttributesChanged(WindowManager.LayoutParams attrs) {
+
+            }
+
+            @Override
+            public void onContentChanged() {
+
+            }
+
+            @Override
+            public void onWindowFocusChanged(boolean hasFocus) {
+
+            }
+
+            @Override
+            public void onAttachedToWindow() {
+
+            }
+
+            @Override
+            public void onDetachedFromWindow() {
+
+            }
+
+            @Override
+            public void onPanelClosed(int featureId, Menu menu) {
+
+            }
+
+            @Override
+            public boolean onSearchRequested() {
+                return false;
+            }
+
+            @Nullable
+            @Override
+            public ActionMode onWindowStartingActionMode(ActionMode.Callback callback) {
+                return null;
+            }
+
+            @Override
+            public void onActionModeStarted(ActionMode mode) {
+
+            }
+
+            @Override
+            public void onActionModeFinished(ActionMode mode) {
+
+            }
+        };
     }
 
     @Override
